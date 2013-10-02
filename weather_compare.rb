3464b4c4de3d45e4a09b933c843c28d3c7498f45
elgrillo83 <elgrillo83@gmail.com>
2013-10-02 22:54:35 +0200

    add forecast methods via wunderkind gem and plain rest call

--- weather_compare.rb
+++ weather_compare.rb
@@ -1,7 +1,30 @@
 require 'sinatra'
 require 'haml'
 require 'wunderground'
+require 'open-uri'
+require 'json'
+
+WUNDERGROUND_API_KEY = '3ba7b015d08f9935'
 
 get '/' do
+  @forecast = forecast_open_uri
   haml :index
+end
+
+def w_api
+  Wunderground.new(WUNDERGROUND_API_KEY)
+end
+
+def forecast_open_uri
+  open('http://api.wunderground.com/api/3ba7b015d08f9935/geolookup/conditions/q/Germany/Berlin.json') do |f|
+    json_string = f.read
+    parsed_json = JSON.parse(json_string)
+    location = parsed_json['location']['city']
+    temp_c = parsed_json['current_observation']['temp_c']
+    return "Current temperature in #{location} is: #{temp_c}\n"
+  end
+end
+
+def forecast_wunderground
+  w_api.forecast_for('Germany', 'Berlin')
 end
